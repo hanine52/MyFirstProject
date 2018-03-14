0f4879427460aeb350aaa6e5a1299e141232c25c
hanine <haniiny2010ma@gmail.com>
2018-03-14 10:10:33 +0100

    posts on arrive d'afficher l image ainsi ...

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use App\Http\Requests\PostsCreateRequest;
 use App\Photo;
 use App\Post;
@@ -30,7 +31,8 @@ class AdminPostsController extends Controller
      */
     public function create()
     {
-        return view('admin.posts.create');
+        $categories = Category::lists('name','id')->all();
+        return view('admin.posts.create', compact('categories'));
 
     }
 
@@ -46,7 +48,7 @@ class AdminPostsController extends Controller
         $user = Auth::user();
         if($file = $request->file('photo_id')){
             $name = time() . $file->getClientOriginalName();
-            $file->move('images', $name);
+            $file->move('image', $name);
             $photo = Photo::create(['file'=>$name]);
             $input['photo_id'] = $photo->id;
         }
